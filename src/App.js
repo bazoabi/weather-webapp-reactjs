3c24f8c457122507b29ef50afd871b8d37c3df86
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-19 13:28:22 +0200

    Adding Rubik font, then apply it to the App component using Theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+//Create theme for Rubik font
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Rubik"],
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Typography variant="h1" gutterBottom>
+            כתיבת טקסט
+          </Typography>
+        </header>
+      </ThemeProvider>
     </div>
   );
 }
